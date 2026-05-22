609dcd782c08832f35cfd55d43fe41d63830e174
orionspark-os <orionspark.in@gmail.com>
2026-05-22 14:03:28 +0530

    test_repo_2

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -32,7 +32,7 @@ export default function Register({ isOpen, onClose, onLoginSuccess }) {
     setLoading(true)
 
     try {
-      const res = await fetch('http://localhost:5000/auth', {
+      const res = await fetch('http://44.222.172.166:5000', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
